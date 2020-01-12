b7e26c68c6088ac139f9db216b47197852bb8671
hawkguy1 <andy.harringa@gmail.com>
2020-01-12 13:37:57 -0600

    Updated code

--- Project02/src/app/components/login/login.component.ts
+++ Project02/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MusicService } from '../../services/music.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private musicService: MusicService) { }
 
+  any: string;
   ngOnInit() {
+    this.get();
   }
 
+  get() {
+    this.musicService.getSomething().subscribe(any => this.any = any);
+  }
 }
--- Project02/src/app/services/music.service.ts
+++ Project02/src/app/services/music.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MusicService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getSomething(): Observable<any> {
+    return this.httpClient.get("http://localhost:3031/nam/home", { responseType: "text" }) as Observable<any>;
+  }
 }
