955395eeceed1a842f3750df4ed711aa169087dc
hawkguy1 <andy.harringa@gmail.com>
2020-01-11 20:00:50 -0600

    Modified models

--- Project02/src/app/models/MouritsResult.ts
+++ Project02/src/app/models/MouritsResult.ts
@@ -5,36 +5,4 @@ export class MouritsResult {
   private result: SongResult;
   private artist: string;
   private songname: string;
-
-  constructor(success: boolean, result: SongResult, artist: string, songname: string) {
-    this.success = success;
-    this.result = result;
-    this.artist = artist;
-    this.songname = songname;
-  }
-
-  getSuccess(): boolean {
-    return this.success;
-  }
-  setSuccess(success: boolean) {
-    this.success = success;
-  }
-  getResult(): SongResult {
-    return this.result;
-  }
-  setResult(result: SongResult) {
-    this.result = result;
-  }
-  getArtist(): string {
-    return this.artist;
-  }
-  setArtist(artist: string) {
-    this.artist = artist;
-  }
-  getSongname(): string {
-    return this.songname;
-  }
-  setSongname(songname: string) {
-    this.songname = songname;
-  }
 }
--- Project02/src/app/models/SongResult.ts
+++ Project02/src/app/models/SongResult.ts
@@ -1,13 +1,3 @@
 export class SongResult {
   private lyrics: string;
-
-  constructor(lyrics: string) {
-    this.lyrics = lyrics;
-  }
-  getLyrics(): string {
-    return this.lyrics;
-  }
-  setLyrics(lyrics: string) {
-    this.lyrics = lyrics;
-  }
 }
