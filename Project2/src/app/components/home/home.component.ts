b1a740d44ba222f3bbec155937bfea7a1e7cc5ee
Discorn <kevin_tiller@live.com>
2020-01-12 21:48:20 -0600

    login auth

--- Project2/src/app/components/home/home.component.ts
+++ Project2/src/app/components/home/home.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public currentUser;
+  constructor( ) {
+    this.currentUser = localStorage.getItem('currentUser')?JSON.parse(localStorage.getItem('currentUser')):'';
+   }
 
   ngOnInit() {
   }
--- Project2/src/app/services/authentication.service.ts
+++ Project2/src/app/services/authentication.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import {Account} from '../models/Account';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor() { }
+  private currentUserSubject:BehaviorSubject<Account>;
+  public currentUser: Observable<Account>;
+ 
+constructor(private http: HttpClient) {
+this.currentUserSubject = new BehaviorSubject<Account>(JSON.parse(localStorage.getItem('currentUser')));
+this.currentUser = this.currentUserSubject.asObservable();
+}
+ 
+public get currentUserValue(): Account {
+return this.currentUserSubject.value;
+}
+ 
+login(username: string, password: string) {
+return this.http.post<any>(`http://localhost:3031/nam/login/`+username+'/'+password, { username, password })
+.pipe(map(user => {
+if (user && user.token) {
+// store user details in local storage to keep user logged in
+localStorage.setItem('currentUser', JSON.stringify(user.result));
+this.currentUserSubject.next(user);
+}
+ 
+return user;
+}));
+}
+ 
+logout() {
+// remove user data from local storage for log out
+localStorage.removeItem('currentUser');
+this.currentUserSubject.next(null);
+}
 }
